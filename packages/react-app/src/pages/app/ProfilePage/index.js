f4169033c525a4d86ea15de24d21921695c44158
e-roy <ericroupe@gmail.com>
2021-12-13 22:06:18 -0500

    added profile page

--- packages/react-app/src/pages/app/ProfilePage/index.js
+++ packages/react-app/src/pages/app/ProfilePage/index.js
@@ -1,5 +1,63 @@
+import { useEffect, useState } from "react";
+import { useEnsData } from "@/hooks/useEnsData";
+import { ethers } from "ethers";
+
+import Davatar from "@davatar/react";
+
 const ProfilePage = () => {
-  return <div className="">User Profile</div>;
+  const [address, setAddress] = useState("");
+  const [provider, setProvider] = useState({});
+
+  const {
+    name,
+    email,
+    url,
+    description,
+    discord,
+    github,
+    reddit,
+    twitter,
+    telegram,
+  } = useEnsData({
+    provider: provider,
+    address,
+  });
+
+  useEffect(() => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+    const getAccount = async () => {
+      const accounts = await provider.listAccounts();
+      setAddress(accounts[0]);
+    };
+    return getAccount();
+  }, []);
+
+  return (
+    <div className="">
+      <div>
+        <div className="m-8">
+          <div className="flex">
+            {address && (
+              <Davatar size={120} provider={provider} address={address} />
+            )}
+
+            <div className="p-12 font-semibold text-gray-700 text-3xl">
+              ENS Name : {name}
+            </div>
+          </div>
+          <div>Email: {email}</div>
+          <div>URL: {url}</div>
+          <div>Description: {description}</div>
+          <div>Discord: {discord}</div>
+          <div>Github: {github}</div>
+          <div>Reddit: {reddit}</div>
+          <div>Twitter: {twitter}</div>
+          <div>Telegram: {telegram}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
